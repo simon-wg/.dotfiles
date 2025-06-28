5d0739a5935d94fe8f28acc1023376aefc9f46e8
Simon Westlin Green <simon.green@live.se>
2025-06-28 16:27:52 +0200

    adds zoxide and starship to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Created by `pipx` on 2025-05-24 10:10:28
+set PATH $PATH /home/simon-wg/.local/bin
+
+starship init fish | source
+zoxide init fish | source
