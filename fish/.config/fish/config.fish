8859b758814f2044541410350d9de95778953a47
Simon Westlin Green <simon.green@live.se>
2025-07-30 20:55:36 +0200

    cleanup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,20 +7,22 @@ set EDITOR nvim
 starship init fish | source
 zoxide init fish | source
 
+# Environment variables
+set -x BUN_INSTALL "$HOME/.bun"
+set -x EDITOR "nvim"
+set -x AZURE_RESOURCE_NAME "sabina-ai-swedencentral"
+
 switch $hostname
     case '5CG04891L4'
-        fish_add_path ~/.opencode/bin
-        fish_add_path ~/.opencode/bin
-        fish_add_path ~/.pyenv/bin
+		set -x PYENV_ROOT "$HOME/.pyenv"
+        fish_add_path $HOME/.opencode/bin
+        fish_add_path $HOME/.opencode/bin
+        fish_add_path $HOME/.pyenv/bin
     case '*'
-		fish_add_path ~/.local/share/gem/ruby/3.4.0/bin
-		fish_add_path ~/.local/share/gem/ruby/3.4.0/bin
+		fish_add_path $HOME/.local/share/gem/ruby/3.4.0/bin
+		fish_add_path $HOME/.local/share/gem/ruby/3.4.0/bin
 end
 
-fish_add_path ~/.local/bin
-fish_add_path ~/.bun/bin
-fish_add_path ~/.cargo/bin
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
+fish_add_path $HOME/.local/bin
+fish_add_path $BUN_INSTALL/bin
+fish_add_path $HOME/.cargo/bin
