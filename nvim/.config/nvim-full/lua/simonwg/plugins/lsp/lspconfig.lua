dc3c3bace3fa86a86704711b6c72eae0e3a61eb0
Simon Westlin Green <simonwestlingreen@proton.me>
2026-02-12 20:15:27 +0100

    refactor(nvim): move lsp related stuff into its own directory

--- nvim/.config/nvim-full/lua/simonwg/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim-full/lua/simonwg/plugins/lsp/lspconfig.lua
@@ -298,22 +298,46 @@ return {
           -- cmd = { ... },
           -- filetypes = { ... },
           -- capabilities = {},
+          on_init = function(client)
+            if client.workspace_folders then
+              local path = client.workspace_folders[1].name
+              if
+                vim.uv.fs_stat(path .. '/.luarc.json')
+                or vim.uv.fs_stat(path .. '/.luarc.jsonc')
+              then
+                return
+              end
+            end
+
+            client.config.settings.Lua =
+              vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                runtime = {
+                  version = 'LuaJIT',
+                },
+                -- Make the server aware of Neovim runtime files
+                workspace = {
+                  checkThirdParty = false,
+                  library = {
+                    vim.env.VIMRUNTIME,
+                    vim.fn.stdpath 'config',
+                    -- "${3rd}/luv/library"
+                    -- "${3rd}/busted/library",
+                  },
+                  -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues with duplicate assets
+                  -- library = vim.api.nvim_get_runtime_file("", true)
+                },
+              })
+          end,
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
+              diagnostics = {
+                globals = { 'vim' },
+              },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
-            },
-            -- INFO: This snippet is in order to let lazylua work with newer lua_ls versions
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME,
-                { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-                -- { path = "snacks.nvim", words = { "Snacks" } }, -- others you need to load
-              },
             },
           },
         },
